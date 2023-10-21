db44b4a80167752fd4bfe80677599fb03d46498a
frankops11 <francmarinc@gmail.com>
2023-10-21 18:49:41 -0500

    feat(navigation): learn fundamentals for about how create custom menu

--- src/navigators/DrawerNavigator.tsx
+++ src/navigators/DrawerNavigator.tsx
@@ -1,13 +1,36 @@
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import {
+  DrawerContentComponentProps,
+  DrawerContentScrollView,
+  createDrawerNavigator,
+} from '@react-navigation/drawer';
 import React from 'react';
 import StackNavigator from './StackNavigator';
 import Settings from '../screens/Settings';
+import {Text, TouchableOpacity, View} from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
+const DrawerContent = (props: DrawerContentComponentProps) => {
+  console.log(props);
+  return (
+    <DrawerContentScrollView>
+      <View>
+        <Text>Navigation App</Text>
+      </View>
+
+      {/* Menu */}
+      <TouchableOpacity
+        onPress={() => props.navigation.navigate('StackNavigator')}>
+        <Text>StackNavigator</Text>
+      </TouchableOpacity>
+      {/* Menu */}
+    </DrawerContentScrollView>
+  );
+};
+
 const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator drawerContent={DrawerContent}>
       <Drawer.Screen name="StackNavigator" component={StackNavigator} />
       <Drawer.Screen name="Settings" component={Settings} />
     </Drawer.Navigator>
